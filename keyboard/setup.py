2001c5d36da01aee831270a36117453e843df7ea
So Yeon Min <symin95@mit.edu>
2018-08-23 15:50:41 -0400

    sample setup.py

--- keyboard/setup.py
+++ keyboard/setup.py
@@ -1,8 +1,8 @@
 import sys
 from cx_Freeze import setup, Executable
 
-build_exe_options = {"packages": ["broderclocks","dtree","config","kconfig","pre_broderclocks_pyqt","widgets","subWindows","mainWindow","string","time","PyQt4.QtGui","PyQt4.QtCore","numpy","sys","cPickle","random","os"],
-                     "include_files": ["data", "save", "corpus", "phrases", "icons", 'user_preferences']}#"pygame",
+build_exe_options = {"packages": ["new_broderclocks","dtree","config","kconfig","widgets","new_subWindows","mainWindow","string","time","PyQt4.QtGui","PyQt4.QtCore","numpy","sys","cPickle","random","os"],
+                     "include_files": ["data", "save", "corpus", "phrases", "icons", 'user_preferences',"../kernel_density_estimation/pretraining_inference.py","../kernel_density_estimation/clock_util.py","../kernel_density_estimation/clock_inference_engine.py"]}#"pygame",
 
 base = None
 
@@ -16,7 +16,7 @@ setup(name = "Nomon",
       version = "1.0",
       description = "Add description",
       options = {"build_exe": build_exe_options},
-      executables = [Executable("keyboard.py", base=base,
+      executables = [Executable("new_keyboard.py", base=base,
                                 icon="nomon.ico",
                                 shortcutName="Nomon",
                                 shortcutDir="DesktopFolder",)])
